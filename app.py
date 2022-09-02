91e899e069655cf169943bc97c4af4f231b38c2f
Snehit Sah <snehitsah@protonmail.com>
2022-09-02 23:37:47 +0530

    Landing page
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, send_from_directory, request
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def index():
+    return ("Server is working")
+
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
