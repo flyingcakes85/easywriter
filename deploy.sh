424d29ca044ffbc6d151138178de124dfaaa4320
Snehit Sah <snehitsah@protonmail.com>
2022-09-03 16:07:52 +0530

    Update deploy script
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@
 # $2 -> url
 
 cd /tmp 
-git clone https://$1@github.com/$2 > /tmp/log 2>&1
-cd easywriter > /tmp/log 2>&1
-cp app.py /home/chococandy/easywriter/app.py > /tmp/log 2>&1
+git clone https://$1@github.com/$2
+cd easywriter
+cp app.py /home/chococandy/easywriter/app.py
 cp deploy.sh /home/chococandy/easywriter/deploy.sh
-cp templates /home/chococandy/templates -r > /tmp/log 2>&1
-cp static /home/chococandy/static -r > /tmp/log 2>&1
+cp templates /home/chococandy/templates -r
+cp static /home/chococandy/static -r
 cd ..
 rm -rf easywriter
 
-sleep 2 > /tmp/log 2>&1
+sleep 2
 
-sudo systemctl restart easywriter > /tmp/log 2>&1
+sudo systemctl restart easywriter
