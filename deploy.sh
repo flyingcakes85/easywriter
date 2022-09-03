56b618a4ec5c9abb3f2d9e6c7d8a1d04c9051ba4
Snehit Sah <snehitsah@protonmail.com>
2022-09-03 16:14:28 +0530

    Works
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,10 @@ git clone https://$1@github.com/$2
 cd easywriter
 cp app.py /home/chococandy/easywriter/app.py
 cp deploy.sh /home/chococandy/easywriter/deploy.sh
-cp templates /home/chococandy/templates -r
-cp static /home/chococandy/static -r
+cp templates /home/chococandy/easywriter/ -r
+cp static /home/chococandy/easywriter/ -r
 cd ..
-# rm -rf easywriter
+rm -rf easywriter
 
 sleep 2
 
